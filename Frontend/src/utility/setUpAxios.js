daf67310d96a4d567d1e38d555b9641a8ce60611
Awarth <adarshtiwari8808@gmail.com>
2024-11-19 19:28:30 +0530

    first push

---
+++ Frontend/src/utility/setUpAxios.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { store } from "../redux/store";
+import { updateAccessToken, logout } from "../redux/slices/authSlice";
+
+const api = axios.create({
+  baseURL: "http://localhost:8000/api/v1",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.auth.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const state = store.getState();
+    const refreshToken = state.auth.refreshToken;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      refreshToken &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await axios.post("https://your-api-url.com/refresh", {
+          refreshToken,
+        });
+        const newAccessToken = response.data.accessToken;
+
+        store.dispatch(updateAccessToken(newAccessToken));
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        store.dispatch(logout());
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
